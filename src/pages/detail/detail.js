d1243eee199e12835efa5bdad1ef8f88854b9951
未来_加油 <741804631@qq.com>
2019-04-09 17:36:33 +0800

    未保存

--- src/pages/detail/detail.js
+++ src/pages/detail/detail.js
@@ -4,18 +4,27 @@ import { getNode } from '../../utils/utils'
 
 export default class Detail extends Component{
 
+    state = {
+        detail: ''
+    }
+
     componentWillMount(){
         const id = this.$router.params.id;
         console.log(id);
         getNode('topic/'+id).then( res => {
             console.log(res);
+            this.setState({
+                detail: res.data.data
+            })
         })
     }
 
     render(){
+        const { detail } = this.state; 
         return(
             <View>
-                detail
+                <View className='acticle_h1'>{ detail.title }</View>
+                
             </View>
         )
     }
